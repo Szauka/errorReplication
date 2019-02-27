8caa062af4fe30c672d6eee4b105594fc3ebd527
romain.lenzotti <rom.lenzotti@gmail.com>
2019-02-27 08:38:39 +0100

    fix: Fix and Update unit test

--- src/middlewares/multipart-file.middleware.ts
+++ src/middlewares/multipart-file.middleware.ts
@@ -1,21 +1,26 @@
-import { EndpointInfo, EndpointMetadata, IMiddleware, OverrideMiddleware, Req, Res, ServerSettingsService } from '@tsed/common';
-import { MultipartFileMiddleware } from '@tsed/multipartfiles';
+import {
+    EndpointInfo,
+    EndpointMetadata,
+    IMiddleware,
+    OverrideMiddleware,
+    Req,
+    Res,
+    ServerSettingsService
+} from '@tsed/common';
+import {MultipartFileMiddleware} from '@tsed/multipartfiles';
 import * as Express from 'express';
 import * as multer from 'multer';
-import * as Path from 'path';
-import { BadRequest } from 'ts-httpexceptions';
-import mkdirp = require('mkdirp');
+import {BadRequest} from 'ts-httpexceptions';
 
 @OverrideMiddleware(MultipartFileMiddleware)
 export class GTArchiveMultipartFileMiddleware implements IMiddleware {
 
     private multer: any = multer;
 
-    constructor(private serverSettingsService: ServerSettingsService) { }
+    constructor(private serverSettingsService: ServerSettingsService) {
+    }
 
     use(@EndpointInfo() endpoint: EndpointMetadata, @Req() request: Express.Request, @Res() response: Express.Response) {
-
-
         if (1 === 1) {
             throw new BadRequest(`This should be thrown and seen on the test failure.`);
         }
@@ -23,14 +28,14 @@ export class GTArchiveMultipartFileMiddleware implements IMiddleware {
 
         const dest = this.serverSettingsService.uploadDir;
         const conf = endpoint.store.get(MultipartFileMiddleware);
-        const options = Object.assign({ dest }, this.serverSettingsService.get('multer') || {}, conf.options || {});
+        const options = Object.assign({dest}, this.serverSettingsService.get('multer') || {}, conf.options || {});
 
 
         return new Promise((resolve, reject) => {
             const onResponse = (err: any) => (err ? reject(err) : resolve());
 
             if (!conf.any) {
-                const fields = conf.fields.map(({ name, maxCount }: any) => ({ name, maxCount }));
+                const fields = conf.fields.map(({name, maxCount}: any) => ({name, maxCount}));
 
                 return this.multer(options).fields(fields)(request, response, onResponse);
             }
